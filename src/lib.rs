c86d56102da48679f7c42b2ddb82cad6af830e52
Shinji Teramoto <shinji.toys@gmail.com>
2021-05-31 14:46:46 +0900

    add ch04

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,4 @@
 pub mod configuration;
 pub mod routes;
 pub mod startup;
-
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use std::net::TcpListener;
+pub mod telemetry;
